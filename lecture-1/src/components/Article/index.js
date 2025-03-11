50c0c57f4db0d6d55a5b3090cfeebe025f07213b
ITHPARK <ithpark1035@gmail.com>
2025-03-11 16:59:13 +0900

    Chapter1: 코드 분할

--- lecture-1/src/components/Article/index.js
+++ lecture-1/src/components/Article/index.js
@@ -16,7 +16,7 @@ function getParametersForUnsplash({ width, height, quality, format }) {
  * 파라미터로 넘어온 문자열에서 일부 특수문자를 제거하는 함수
  * (Markdown으로 된 문자열의 특수문자를 제거하기 위함)
  * */
-function removeSpecialCharacter(sdsadsatr) {
+function removeSpecialCharacter(str) {
     // const removeCharacters = [
     //     "#",
     //     "_",
